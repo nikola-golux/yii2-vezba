c1496e2cb121d60a43ecb16341466e16db629735
Nikola Golubovic <nikola.golux@gmail.com>
2017-04-26 09:45:50 +0200

    Dodata konfiguracija za LOCAL ENTEL

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=entel',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=ngzadatak',
+    'username' => 'ngolubovic',
+    'password' => 'nikola2351',
     'charset' => 'utf8',
 ];
