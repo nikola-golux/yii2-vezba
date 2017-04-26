b0c876da30f5a70387149408e518c41e4b4d879f
Nikola <nikola.golux@gmail.com>
2017-04-26 07:50:18 +0000

    Dodata konfiguracija za local, sada mozemo da biramo konfiguraciju iz konfiguracionih fajlova

---
+++ config/db_local_entel.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ngzadatak',
+    'username' => 'ngolubovic',
+    'password' => 'nikola2351',
+    'charset' => 'utf8',
+];
